7212a1844d4a75c87b0927c57ec783996dd5ce4e
demey283 <demeybuilders@gmail.com>
2025-11-19 15:36:49 +0100

    JsonData Versturen

--- Esp32/boot.py
+++ Esp32/boot.py
@@ -1,7 +1,7 @@
 import time
 from machine import Pin
 import onewire, ds18x20
-
+import json
 data_pin = Pin(18)  
 
 ow = onewire.OneWire(data_pin)
@@ -10,11 +10,12 @@ ds = ds18x20.DS18X20(ow)
 
 roms = ds.scan()
 print("Gevonden sensoren:", roms)
-
+print("test")
 while True:
     ds.convert_temp()      
     time.sleep_ms(750)      
     for rom in roms:
         temp = ds.read_temp(rom)
-        print("Temperatuur:", temp, "°C")
+        data ={"Temperatuur": temp}
+        print(json.dumps(data))
     time.sleep(1)
