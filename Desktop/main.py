8a6947dbe623d3946b04fe44692f31e3e7dfd72c
demey283 <demeybuilders@gmail.com>
2025-11-19 15:29:14 +0100

    Seriele data proberen lezen

--- Desktop/main.py
+++ Desktop/main.py
@@ -1,1 +1,12 @@
-print("hello")
+import serial
+import json
+
+ser = serial.Serial("COM3", 115200) 
+
+while True:
+    line = ser.readline().decode().strip()
+    try:
+        data = json.loads(line)
+        print("Ontvangen:", data)
+    except:
+        pass
